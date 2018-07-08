2145b025b905e84eed79d5eb9278fee57cce4f90
Luminary-S <buaasgl@gmail.com>
2018-07-09 05:57:48 +0800

    add .gitignore file

--- nodes/cam_process.py
+++ nodes/cam_process.py
@@ -6,6 +6,19 @@
 
 import cv2
 
+def callback(image, camera_info):
+#Solve all of perception here...
+    pass
+
+
+
+
 
 if __name__=="__main__":
-    pass
+    image_sub = message_filters.Subscriber('image', Image)
+    imu_sub = message_filters.Subscriber('imu', Imu)
+
+    ts = message_filters.TimeSynchronizer([image_sub, imu_sub], 10)
+    ts.registerCallback(callback)
+    rospy.spin()
+
